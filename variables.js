d066e02c28174ba50eb2d69c78d219434bdb0254
danielvinzia <danielvinzia@gmail.com>
2022-02-02 10:35:29 -0300

    clase de variables concluida

---
+++ variables.js
@@ -0,0 +1,19 @@
+var nombre = "Osvald";
+
+// declarar 
+var edad;
+
+edad
+// inicializar
+edad = 30;
+
+edad
+
+var elementos = ["computadora", "celular"];
+
+var persona = {
+    nombre: "diego",
+    edad: 30
+}
+
+persona;
