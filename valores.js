ab2dbdd5ebf8efa6d6608ae5f2368a02aad1d8aa
danielvinzia <danielvinzia@gmail.com>
2022-02-02 10:20:07 -0300

    primer clase definimos valores

---
+++ valores.js
@@ -0,0 +1,15 @@
+//valores primitivos
+//numero
+40
+//cadena de texto
+    "daniel vinzia"
+    //booleano
+true
+false
+//valores vacios
+null
+undefined //evitarlo
+
+//valores tipo objeto
+    [1, 2, 3] //arreglo
+    { nombre: "Diego" }
