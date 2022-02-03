1d7f0ecbabfe91ea37346e716dc85a9b9739187a
danielvinzia <danielvinzia@gmail.com>
2022-02-03 13:34:12 -0300

    primera clase de recorrido de arreglos

---
+++ recorrido_de_Arrays.js
@@ -0,0 +1,19 @@
+var articulos = [
+    { nombre: "Bici", costo: 3000 },
+    { nombre: "Tv", costo: 2500 },
+    { nombre: "libro", costo: 320 },
+    { nombre: "Celular", costo: 10000 },
+    { nombre: "Laptop", costo: 20000 },
+    { nombre: "Teclado", costo: 500 },
+    { nombre: "Audifonos", costo: 1700 },
+];
+
+// metodo filter
+var articulosFiltrados = articulos.filter(function(articulo) {
+    return articulo.costo <= 500
+});
+
+// metodo map
+var nombreArticulos = articulos.map(function(articulo) {
+    return articulo.nombre
+});
